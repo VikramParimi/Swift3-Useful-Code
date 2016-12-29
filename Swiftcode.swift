46583f549777bb082251a3479ba94d14ace7e61c
Carpenter, Deepak (US - Bengaluru) <decarpenter@deloitte.com>
2016-12-29 10:50:44 +0530

    Added CheckBox Code

--- Swiftcode.swift
+++ Swiftcode.swift
@@ -1,5 +1,5 @@
 //
-//  Formatter.swift
+//  Swiftcode.swift
 //  Swiftcode
 //
 //  Created by Deepak Carpenter on 2016/12/29.
@@ -13,3 +13,48 @@ self.navigationController?.navigationBar.shadowImage = UIImage()
 self.navigationController?.navigationBar.isTranslucent = true
 self.navigationController?.view.backgroundColor = UIColor.clear
 self.navigationController?.navigationBar.backgroundColor = UIColor.clear
+
+//MARK:- Create Checkbox UIButton
+/*Assign Checkbox class as your UIButton Class and use isChecked wherever you want to check the Checkbox state*/
+class Checkbox: UIButton {
+    // Images
+    var checkedImage = UIImage()
+    var unCheckedImage = UIImage()
+    
+    @IBInspectable
+    var activeImage: UIImage = UIImage() {
+        didSet {
+            checkedImage = activeImage
+        }
+    }
+    
+    @IBInspectable
+    var inActiveImage: UIImage = UIImage() {
+        didSet {
+            unCheckedImage = inActiveImage
+        }
+    }
+    
+    
+    // Bool property
+    var isChecked: Bool = false {
+        didSet{
+            if isChecked == true {
+                self.setImage(checkedImage, for: .normal)
+            } else {
+                self.setImage(unCheckedImage, for: .normal)
+            }
+        }
+    }
+    
+    override func awakeFromNib() {
+        self.addTarget(self, action: #selector(buttonClicked(sender:)), for: UIControlEvents.touchUpInside)
+        self.isChecked = false
+    }
+    
+    func buttonClicked(sender: UIButton) {
+        if sender == self {
+            isChecked = !isChecked
+        }
+    }
+}
